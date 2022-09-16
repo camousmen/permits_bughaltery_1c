aed11c9c8805d766ab1a3ced1520fc430459d452
Andrey Tarasov <camousmen@gmail.com>
2022-09-16 23:39:28 +0900

    Сделал динамический поиск

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-from asyncio.windows_events import NULL
-from atexit import register
-from gc import callbacks
+
+from copy import copy
 from tkinter import END, StringVar, Tk, Frame, BOTH, Listbox, Scrollbar
 from tkinter.ttk import Frame, Button, Style, Label, Entry
+import tkinter as tk
+import csv, pickle
 
 
-partner_list = [x for x in range(0, 100)]
+partner_list = []
 name_list = [x for x in range(0, 100)]
+partners_names_rel_list = []
 
-p_listbox : Listbox
-n_listbox : Listbox
+# для тестов будем загружать из файла csv
+def load_partners_from_csv():
+    with open("partner_list.csv", encoding='utf-8') as r_file:
+        file_reader = csv.reader(r_file, delimiter=";")
+        for row in file_reader:
+            partner_list.append([row[0], row[1]])
 
+# сохранение и загрузка списка партнеров и связанных лиц
+def save_partners_names_rel_to_pickle():
+    try:
+        with open("data.pickle", "wb") as f:
+            pickle.dump(partners_names_rel_list, f, protocol=pickle.HIGHEST_PROTOCOL)
+    except Exception as ex:
+        print("Error picking object:", ex)
 
-# методы для динамического поиска в контрагентах и именах
-def partner_do_validation(new_value):
-    print(new_value)
-    p_listbox.delete()
-    return True
-
-def name_do_validation(new_value):
-    print(new_value)
-    return True
+def load_partners_names_rel_from_pickle():
+    try:
+        with open("data.pickle", "rb") as f:
+            partners_names_rel_list = pickle.load(f)
+    except Exception as ex:
+        print("Error unpuck:", ex)
 
 
 # собственно сам класс главного окна приложения
@@ -35,7 +45,7 @@ class Example(Frame):
         self.initUI()
     
     def centerWindow(self):
-        w = 500
+        w = 600
         h = 350
 
         sw = self.parent.winfo_screenwidth()
@@ -45,7 +55,6 @@ class Example(Frame):
         y = (sh - h) / 2
         self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
     
-
     def initUI(self):
         self.parent.title("Печать разрешений")
         self.style = Style()
@@ -61,52 +70,80 @@ class Example(Frame):
         label3 = Label(self, text="Сумма")
 
         label1.place(x=20, y=20)
-        label2.place(x=140, y=20)
-        label3.place(x=240, y=20)
+        label2.place(x=350, y=20)
+        label3.place(x=500, y=20)
 
         # фильтрация по спискам при изменении полей ввода
         p_sv = StringVar(self)
         n_sv = StringVar(self)
-        
+
         # два поля для фильтрации списков
-        vcmd_p = (self.register(partner_do_validation), '%P')
-        vcmd_n = (self.register(name_do_validation), '%P')
-        find_partner_entry = Entry(self, width=15, textvariable=p_sv, validate="key", validatecommand=vcmd_p)
-        find_name_entry = Entry(self, width=15, textvariable=n_sv, validate="key", validatecommand=vcmd_n)
-        summ_entry = Entry(self, width=15)
+        vcmd_p = (self.register(self.partner_do_validation), '%P')
+        vcmd_n = (self.register(self.name_do_validation), '%P')
+        find_partner_entry = Entry(self, width=20, textvariable=p_sv, validate="key", validatecommand=vcmd_p, font=16)
+        find_name_entry = Entry(self, width=10, textvariable=n_sv, validate="key", validatecommand=vcmd_n, font=16)
+        summ_entry = Entry(self, width=10)
 
         find_partner_entry.place(x=20, y=40)
-        find_name_entry.place(x=140, y=40)
-        summ_entry.place(x=240, y=40)
+        find_name_entry.place(x=350, y=40)
+        summ_entry.place(x=500, y=40)
+
+        # кнопка добавления нового лица
+        add_name_button = tk.Button(self, text="+", command=self.click_add_name_button, width="3", height="1")
+        add_name_button.place(x=450, y=40)
 
         # списки с контрагентами и лицами
-        partner_listbox = Listbox(self)
-        name_listbox = Listbox(self)
+        self.partner_listbox = Listbox(self, width=45)
+        self.name_listbox = Listbox(self)
 
         for el in partner_list:
-            partner_listbox.insert(END, el)
+            self.partner_listbox.insert(END, f"{el[0]}/{el[1]}")
 
         for el in name_list:
-            name_listbox.insert(END, el)
+            self.name_listbox.insert(END, el)
 
-        partner_listbox.place(x=20, y=60)
-        name_listbox.place(x=140, y=60)
+        self.partner_listbox.place(x=20, y=70)
+        self.name_listbox.place(x=350, y=70)
 
-        p_listbox = partner_listbox
-        n_listbox = name_listbox
-        
         # скроллбары для списков
         plbox_scrollbar = Scrollbar(self, orient="vertical")
-        plbox_scrollbar.config(command=partner_listbox.yview)
+        plbox_scrollbar.config(command=self.partner_listbox.yview)
         plbox_scrollbar.pack(side="right", fill="y")
 
         nlbox_scrollbar = Scrollbar(self, orient="vertical")
-        nlbox_scrollbar.config(command=name_listbox.yview)
+        nlbox_scrollbar.config(command=self.name_listbox.yview)
         nlbox_scrollbar.pack(side="right", fill="y")
+    
+    def click_add_name_button(self):
+        print("add")
 
+    # методы для динамического поиска в контрагентах и именах
+    def partner_do_validation(self, new_value):
+        print(len(new_value))
+        if len(new_value) != 0:
+            if len(new_value) < 2:
+                self.partner_listbox.delete(0, END)
+                for el in partner_list:
+                    self.partner_listbox.insert(END, f"{el[0]}/{el[1]}")
+            elif len(new_value) > 2:
+                self.partner_listbox.delete(0, END)
+                for el in partner_list:
+                    buf_el = el[0].lower()
+                    new_value = new_value.lower()
+                    if buf_el.find(new_value) != -1:
+                        self.partner_listbox.insert(END, el[0])
+
+
+        return True
+
+    def name_do_validation(self, new_value):
+        print(new_value)
+        return True
 
 def main():
     root = Tk()
+    load_partners_names_rel_from_pickle()
+    load_partners_from_csv()
     ex = Example(root)
     root.mainloop()
 
